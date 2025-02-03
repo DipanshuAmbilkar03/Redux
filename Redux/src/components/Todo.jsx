967d89cb6531a2382cf9edce048df03e552cecf7
DipanshuAmbilkar03 <dipanshuambilker@gmail.com>
2025-02-04 00:00:23 +0530

    1) dont use { --- } for deconstruction of object. 2) Render the state variable in main jsx

--- Redux/src/components/Todo.jsx
+++ Redux/src/components/Todo.jsx
@@ -1,13 +1,18 @@
 import { useSelector } from "react-redux"
 
 export default function Todo() {
-    const todos = useSelector((state) => {
-        state.todos;
-    })
+    const todos = useSelector((state) => state.todos)
     console.log(todos);
     return(
         <div>
             <h2>Todo Redux</h2>
+            <ul>
+                { todos.map((todos) => 
+                    <li key={todos.id}>
+                        {todos.task}
+                    </li>
+                )}
+            </ul>
         </div>
     )
 }
