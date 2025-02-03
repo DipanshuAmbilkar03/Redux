67caea71cb6c455ec3c7126567156ebef23aa6dc
DipanshuAmbilkar03 <dipanshuambilker@gmail.com>
2025-02-03 23:06:49 +0530

    import the shit

--- Redux/src/app/stote.js
+++ Redux/src/app/stote.js
@@ -1,7 +1,8 @@
 import { createSlice, configureStore } from '@reduxjs/toolkit'
+import todoReducers from "../features/Todo/todo";
 
 const store = configureStore({
-    reducers: {},
+    reducers: todoReducers,
 });
 
 export default store;
